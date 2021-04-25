e9790e8fb15ea4874b7aa1df830affd0ddf3e7b6
yann <tmp4321@qq.com>
2021-04-25 14:44:52 +0800

    base module cvm with public ip

--- instance/main.tf
+++ instance/main.tf
@@ -10,12 +10,19 @@ terraform {
 
 resource "tencentcloud_instance" "cvm" {
   image_id = "${var.tc_image_id}"
-
   instance_type = "${var.tc_instance_type}"
-  
   availability_zone = "${var.tc_az}"
-
   system_disk_type = "CLOUD_PREMIUM"
+  
+  project_id         = "${var.tc_project_id}"
+  allocate_public_ip = "${var.tc_enable_public_ip}"
+  # security_groups = "${var.tc_security_group_id}"
+  internet_max_bandwidth_out = "${var.tc_bandwidth_out}"
+  user_data = "${var.tc_user_data}"
 }
 
 
+output "public_ip" {
+  value       = tencentcloud_instance.cvm.public_ip
+  description = "The public IP of the Instance"
+}
--- instance/variables.tf
+++ instance/variables.tf
@@ -2,3 +2,8 @@
 variable "tc_az" {}
 variable "tc_image_id" {}
 variable "tc_instance_type" {}
+
+variable "tc_project_id" {}
+variable "tc_enable_public_ip" {}
+variable "tc_bandwidth_out" {}
+variable "tc_user_data" {}
